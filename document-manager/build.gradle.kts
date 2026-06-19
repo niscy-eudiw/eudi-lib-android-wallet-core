niscy-eudiw/eudi-lib-android-wallet-document-manager

refactor: replace DocumentClaim.identifier with format-specific typed fields

document-manager/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
     implementation(libs.cose)
 
     // sd-jwt-vc
-    implementation(libs.eudi.sd.jwt.vc.kt)
+    api(libs.eudi.sd.jwt.vc.kt)
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.content.negotiation)

niscy-eudiw/eudi-lib-android-wallet-core

document-manager/build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     implementation(libs.upokecenter.cbor)
     implementation(libs.cose.java)
 
-    implementation(libs.eudi.lib.jvm.sdjwt.kt)
+    api(libs.eudi.lib.jvm.sdjwt.kt)
 
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.content.negotiation)